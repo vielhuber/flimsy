35b50694cdea62d72cca6d15ed4b7e4aa0f1dfaf
David Vielhuber <david@vielhuber.de>
2018-07-10 00:26:05 +0200

    updated readme

--- flimsy.py
+++ flimsy.py
@@ -19,10 +19,15 @@ def addAbbreviation(word, target):
 
         name = event.name
 
+        if event.event_type == keyboard.KEY_UP or name in keyboard.all_modifiers:
+            return
+
         if timeout and event.time - state.time > timeout:
             state.current = ''
 
         state.time = event.time
+
+        print(state.current);
 
         matched = state.current == word or (match_suffix and state.current.endswith(word))
         if name in triggers and matched:
